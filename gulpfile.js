27b8832e7bbe8801b8d3441a1cd6027d7b4cf486
Chris Campbell <xipolis@gmx.de>
2023-12-29 22:01:15 +0100

    npm update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const sass = require('gulp-sass')(require('sass'));
 const uglify = require("gulp-uglify");
 const cleanCSS = require("gulp-clean-css");
 const sourcemaps = require("gulp-sourcemaps");
